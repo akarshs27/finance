2655b3359827913f818566c603b150f2ed171ebd
Akarsh <akarshs27@gmail.com>
2024-10-02 11:21:30 +0530

    Drizzle fixes

---
+++ drizzle/0000_eager_hulk.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS "accounts" (
+	"id" text PRIMARY KEY NOT NULL,
+	"plaid_id" text,
+	"name" text NOT NULL,
+	"user_id" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "transactions" (
+	"id" text PRIMARY KEY NOT NULL,
+	"amount" integer NOT NULL,
+	"payee" text NOT NULL,
+	"notes" text,
+	"date" timestamp NOT NULL,
+	"account_id" text NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "transactions" ADD CONSTRAINT "transactions_account_id_accounts_id_fk" FOREIGN KEY ("account_id") REFERENCES "public"."accounts"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
